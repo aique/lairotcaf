3d2882cbe6b5e84d5d899ffa0fb668d1d0b184cc
Aique <aiquefernandez@gmail.com>
2024-09-04 19:31:53 +0200

    showing configurator options

--- libs/bicycles-web/feature-configurator/src/lib/bicycles-web-feature-configurator/components/configurator-form.component.ts
+++ libs/bicycles-web/feature-configurator/src/lib/bicycles-web-feature-configurator/components/configurator-form.component.ts
@@ -5,10 +5,34 @@ import { contractSelector } from '../selectors/configurator.selector';
 @Component({
   selector: 'feature-configurator-form',
   template: `
-    <form>
-      <p>{{ options$ | async | json }}</p>
-      <input type="submit" value="I want it!" />
-    </form>
+    <p *ngIf="!(options$ | async)">
+      No hemos podido obtener información sobre el producto 🙏
+    </p>
+    <div *ngIf="options$ | async as options">
+      <form>
+        <div class="form-columns">
+          <div class="form-groups">
+            <div *ngFor="let component of options.components">
+              <div class="form-group">
+                <label>{{ component.name }}</label>
+                <select>
+                  <option *ngFor="let option of component.options">
+                    {{ option.name }}
+                  </option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="submit-area">
+            <input class="submit-buttom" type="submit" value="I want it!" />
+            <span class="price">
+              <span class="price-value">66,38</span> €
+            </span>
+          </div>
+        </div>
+      </form>
+    </div>
   `,
   styleUrl: './configurator-form.component.scss',
 })
--- libs/bicycles-web/feature-configurator/src/lib/bicycles-web-feature-configurator/containers/configurator.container.ts
+++ libs/bicycles-web/feature-configurator/src/lib/bicycles-web-feature-configurator/containers/configurator.container.ts
@@ -5,8 +5,10 @@ import { ConfiguratorActions } from '../actions/configurator.actions';
 @Component({
   selector: 'feature-configurator-container',
   template: `
-    <h3>Configurator</h3>
-    <feature-configurator-form></feature-configurator-form>
+    <div class="content">
+      <h3>Configurator</h3>
+      <feature-configurator-form></feature-configurator-form>
+    </div>
   `,
   styleUrl: './configurator.container.scss',
 })
